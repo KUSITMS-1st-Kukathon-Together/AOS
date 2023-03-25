232bc79e6c334db6330bb956a8f34d3c37863255
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2023-03-25 22:53:54 +0900

    Feat : Home Activity Bottom Navigation

--- app/src/main/java/com/team2/together1/MainActivity.kt
+++ app/src/main/java/com/team2/together1/MainActivity.kt
@@ -2,10 +2,43 @@ package com.team2.together1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.team2.together1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation(){
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, CarefulFragment())
+            .commitAllowingStateLoss()
+
+        binding.bnvMain.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.first -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, CarefulFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.second-> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, RecommendFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+
+            }
+            false
+        }
     }
 }
